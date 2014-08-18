57abfb878ad568737e6d5d1ac3eb471b23b21ef5
mrcodeninja <ben@mrcodeninja.com>
2014-08-18 00:34:57 -0700

    Updated to use explicit reference to function argument names. Updated to use subset() function over [,] notation. Updated plot title to use a subtitle for location information. Removed custom library loading function. Replaced with call to require() function instead.

--- plot3.R
+++ plot3.R
@@ -1,23 +1,14 @@
 create_data_directory <- function() {
     if (!file.exists("data")) {
-        dir.create("data")
+        dir.create(path = "data")
     } 
 }
 
 download_and_unzip <- function(url, destfile) {
     if (!file.exists(destfile)) {
-        download.file(url, destfile, method = "curl")
+        download.file(url = url, destfile = destfile, method = "curl")
     }
     unzip(zipfile = destfile, exdir = "data")
-}
-
-load_libraries <- function() {
-    installed_packages <- installed.packages()[, 1]
-    if (!is.element("ggplot2", installed_packages)) {
-        cat("Installing ggplot2 package...\n")
-        install.packages("ggplot2")
-    }
-    library(ggplot2)
 }
 
 plot3 <- function() {
@@ -25,21 +16,32 @@ plot3 <- function() {
     download_and_unzip(
         url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",
         destfile = "data/exdata-data-NEI_data.zip")
-    load_libraries()
-    
-    NEI <- readRDS("data/summarySCC_PM25.rds")
-    NEI <- NEI[NEI$fips == "24510", c("Emissions", "year", "type")]
+
+    NEI <- readRDS(file = "data/summarySCC_PM25.rds")
+    NEI <- subset(
+        x = NEI,
+        subset = fips == "24510",
+        select = c("Emissions", "year", "type"))
     NEI <- transform(NEI, type = factor(type))
-    emissionsSumByYear <- with(NEI, aggregate(x = Emissions, by = list(year, type), sum))
+    emissionsSumByYear <- with(
+        data = NEI,
+        aggregate(x = Emissions, by = list(year, type), FUN = sum))
     rm(NEI)
     names(emissionsSumByYear) <- c("Year", "Type", "Emissions")
     
-    png("plot3.png")
+    png(filename = "plot3.png")
     
+    require(package = ggplot2)
     g <- ggplot(
         data = emissionsSumByYear,
         aes(Year, Emissions))
-    p <- g + geom_point() + facet_grid(. ~ Type) + geom_smooth(method = "lm") + ggtitle(label = expression("Baltimore City, Maryland " * PM[2.5] * " Emissions Trend")) + ylab(label = "Total Emissions")
+    p <- g +
+        geom_point() +
+        facet_grid(. ~ Type) +
+        geom_smooth(method = "lm") +
+        ggtitle(
+            label = expression(atop(PM[2.5] * " Emissions Trend", atop("Baltimore City, Maryland")))) +
+        ylab(label = "Total Emissions")
     print(p)
     
     dev.off()
