8971f7697708c2a7dc09068bb1898fb3db891247
mrcodeninja <ben@mrcodeninja.com>
2014-08-18 00:30:21 -0700

    Updated to use explicit reference to function argument names. Updated to use subset() function over [,] notation. Updated plot title to use a subtitle for location information.

--- plot1.R
+++ plot1.R
@@ -1,12 +1,12 @@
 create_data_directory <- function() {
     if (!file.exists("data")) {
-        dir.create("data")
+        dir.create(path = "data")
     } 
 }
 
 download_and_unzip <- function(url, destfile) {
     if (!file.exists(destfile)) {
-        download.file(url, destfile, method = "curl")
+        download.file(url = url, destfile = destfile, method = "curl")
     }
     unzip(zipfile = destfile, exdir = "data")
 }
@@ -17,23 +17,27 @@ plot1 <- function() {
         url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",
         destfile = "data/exdata-data-NEI_data.zip")
     
-    NEI <- readRDS("data/summarySCC_PM25.rds")
-    NEI <- NEI[, c("Emissions", "year")]
-    emissionsSumByYear <- with(NEI, aggregate(Emissions, list(year), sum))
+    NEI <- readRDS(file = "data/summarySCC_PM25.rds")
+    NEI <- subset(
+        x = NEI,
+        select = c("Emissions", "year"))
+    emissionsSumByYear <- with(
+        data = NEI,
+        aggregate(x = Emissions, by = list(year), FUN = sum))
     rm(NEI)
     names(emissionsSumByYear) <- c("Year", "Emissions")
 
-    png("plot1.png")
+    png(filename = "plot1.png")
 
     opt <- options(scipen = 100)
     op <- par(mar = c(5, 7, 4, 2) + 0.1)
 
-    with(emissionsSumByYear,
+    with(data = emissionsSumByYear,
          plot(
              x = Year,
              y = Emissions,
              type = "b",
-             main = expression("United States " * PM[2.5] * " Emissions Trend"),
+             main = expression(atop(PM[2.5] * " Emissions Trend", atop("United States"))),
              ylab = "",
              las = 1)
          )
